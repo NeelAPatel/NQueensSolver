b1426af8e1269e62f7284e85489932719f2dc0b4
Neel <send2neelpatel@gmail.com>
2018-10-10 11:26:51 -0400

    Making tables

--- randtest.py
+++ randtest.py
@@ -1,8 +1,61 @@
 import random
 
-print random.randint(0, 7)
-
 arr = []
 for x in range(8):
-	arr.append( random.randint(0,7))
+	arr.append( random.randint(1,8))
 	print(arr)
+
+
+arrTable = [[] for _ in range(8)]
+print arrTable
+
+rowIndex = 0
+while rowIndex <= 7:
+	for x in range(8):
+
+		arrTable[rowIndex].append('')
+	print arrTable
+	rowIndex += 1
+	print("============")
+
+index = 0
+while (index <= 7):
+	rowVal = arr[index]
+	arrTable[rowVal-1][index] = 'Q'
+	index+=1
+
+print arrTable
+
+#Horizontal
+hAttacks = 0
+for index in range(8):
+	selected = arr[index]
+	print("SELECTED VAL: " + str(selected))
+	newRange = 7 - index
+	for newIndex in range(newRange):
+		print("new arr: " + str(arr[index + newIndex+1]))
+		if (arr[index + newIndex+1] == selected):
+			hAttacks+=1
+
+
+
+print("Horizontal attacks: " + str(hAttacks))
+
+#
+#
+# #Diagonal
+# dAttacks = 0
+# for index in range(8):
+# 	currVal = arr[index]
+# 	print("curr VAL: " + str(currVal))
+#
+# 	currCopy = currVal
+# 	newRange = 7 - index
+# 	for newIndex in range(newRange):
+# 		print("new arr: " + str(arr[index + newIndex+1]))
+# 		if (arr[index + newIndex+1] == selected):
+# 			hAttacks+=1
+#
+#
+#
+# [[],[Q,'', '', '', '', '', '', ''],[],[],[],[],[],[]]
